1fab9290a5d9cadf92d773077c94a3ff2b0561d1
YounesDjelloul <younesdjelloul14@gmail.com>
2025-05-12 06:13:45 +0800

    adding admin to installed apps

--- TradeShield/settings.py
+++ TradeShield/settings.py
@@ -29,6 +29,7 @@ load_dotenv()
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
